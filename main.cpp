92a963e10060f8ac6864e91fdcf7610db8bff675
AX-Ray <vandpal@bk.ru>
2026-07-09 18:10:21 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3,15 +3,20 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 int main() {    
+    std::srand(time(nullptr)); 
+
+
     Network net;    
     net.add_layer(std::make_unique<Layer>(1, 15, std::make_unique<LeakyReLU>()));  
     net.add_layer(std::make_unique<Layer>(15, 15, std::make_unique<LeakyReLU>()));
     net.add_layer(std::make_unique<Layer>(15, 1, std::make_unique<Linear>()));       
         
     std::vector<double> inputs;
-    for (double x = -100.0; x < 100.0; x += 0.5) {
+    for (double x = -300.0; x <= 300.0; x += 0.5) {
         inputs.push_back(x);
     }
     std::vector<double> targets;
@@ -19,13 +24,13 @@ int main() {
         targets.push_back(x * 2.0);  
     }    
         
-    double learning_rate = 0.001;  
-    int epochs = 20000; 
+    double learning_rate = 0.01;  
+    int epochs = 30000; 
     
-    //net.load("model_mult2v2.txt");
+    net.load("model_mult2v4.txt");
         
     
-    for (int epoch = 0; epoch < epochs; ++epoch) {
+    /*for (int epoch = 0; epoch < epochs; ++epoch) {
         double total_loss = 0.0;
         
         for (int i = 0; i < inputs.size(); i++) {
@@ -43,16 +48,22 @@ int main() {
         }
     }
 
-    std::string filename = "model_mult2v3.txt";
-    net.save(filename);   
+    std::string filename = "model_mult2v4.txt";
+    net.save(filename);   */
+
+
+    std::ofstream file("predict.txt");
         
-    for (double x = -300.0; x < 300.0; x += 1.0) {
+    for (double x = -100000.0; x <= 100000.0; x += 0.5) {
         Matrix input(1, 1);
         input(0, 0) = x / 100.0;  
         
-        Matrix output = net.predict(input);
-        std::cout << "Input: " << x << " -> Output: " << output(0, 0) * 100.0 << std::endl; 
-    }    
+        Matrix output = net.predict(input);        
+
+        file << "Input: " << x << " -> " << output(0, 0) * 100.0 << std::endl;
+    }   
+    
+    file.close();
     
     return 0;
 }
