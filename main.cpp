b4152dd5f07b936a60603ae52d79d1a87010ffb7
AX-Ray <vandpal@bk.ru>
2026-08-12 15:13:24 +0300

    LR sheduler

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@
 #include <ctime> 
 
 void net_train(Network& net, int epochs, double l_r, const std::vector<double>& inputs, const std::vector<double>& targets) {
+    double prev_total_loss = 999999.0; 
+
     for (int epoch = 0; epoch <= epochs; ++epoch) {
         double total_loss = 0.0;
-
-        if (epoch > 0 && epoch % 10000 == 0) { // Changing learning rate during trainig
-            l_r /= 4.0;            
-        }
-        
+                
         for (int i = 0; i < inputs.size(); i++) {
             Matrix input(1, 1);
             input(0, 0) = inputs[i] / 100.0; 
@@ -26,15 +24,27 @@ void net_train(Network& net, int epochs, double l_r, const std::vector<double>& 
             total_loss += net.train(input, target, l_r);
         }  
         
+        double current_epoch_loss = total_loss / inputs.size();
+        
+        if (epoch > 0 && epoch % 1000 == 0) {            
+            if (current_epoch_loss >= prev_total_loss) {
+                l_r *= 0.75;                 
+            } else {
+                l_r *= 1.1; 
+            }
+            prev_total_loss = current_epoch_loss; 
+        }
+        
         if (epoch % 1000 == 0) {
-            std::cout << "Epoch " << epoch << ", loss: " << total_loss / inputs.size() << std::endl;
+            std::cout << "Epoch " << epoch << ", loss: " << current_epoch_loss << " (LR: " << l_r << ")" << std::endl;
         }
     }
 }
 
+
 void net_init(Network& net) { 
-    net.add_layer(std::make_unique<Layer>(1, 20, std::make_unique<ReLU>()));       
-    net.add_layer(std::make_unique<Layer>(20, 20, std::make_unique<LeakyReLU>(0.01)));       
+    net.add_layer(std::make_unique<Layer>(1, 20, std::make_unique<LSLU>()));       
+    net.add_layer(std::make_unique<Layer>(20, 20, std::make_unique<LSLU>()));       
     net.add_layer(std::make_unique<Layer>(20, 1, std::make_unique<Linear>()));       
 }
 
